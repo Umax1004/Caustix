21a2e9a167730b769602294d7e186f35d73803b1
Umesh Ramchandani <umeshramchandani10@gmail.com>
2019-05-23 18:15:12 +0530

    Implemented window events
    Implemented window and key events

--- Caustix/src/Caustix/Application.cpp
+++ Caustix/src/Caustix/Application.cpp
@@ -1,16 +1,15 @@
 #include "cxpch.h"
 #include "Application.h"
-#include "Events/ApplicationEvent.h"
-#include "Log.h"
-
 #include <GLFW/glfw3.h>
 
 namespace Caustix
 {
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
@@ -21,10 +20,23 @@ namespace Caustix
 	void Application::Run()
 	{
 
-		while (true)
+		while (m_Running)
 		{
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event & e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		CX_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent & e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Caustix/src/Caustix/Application.h
+++ Caustix/src/Caustix/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Caustix
@@ -12,9 +13,10 @@ namespace Caustix
 		virtual ~Application();
 
 		void Run();
-
+		void OnEvent(Event& e);
 	private:
 		std::unique_ptr<Window> m_Window;
+		bool OnWindowClose(WindowCloseEvent& e);
 		bool m_Running = true;
 	};
 
